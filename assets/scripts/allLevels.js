f0eb28955ee4480441a526a263ac47527868874b
Keith1 <155455657+Keith1R0blox@users.noreply.github.com>
2026-08-28 18:06:32 +0700

    ok

---
+++ assets/scripts/allLevels.js
@@ -0,0 +1,44 @@
+window.allLevels = [
+    [
+        "stereo_madness", // internal level name
+	    "Stereo Madness", // proper level name
+	    "level_1",        // level id in assets/levels
+	    "Forever Bound"   // person who made the song
+    ],
+    [
+        "back_on_track",
+	    "Back on Track",
+	    "level_2",
+	    "DJVI" 
+    ],
+    [
+        "polargeist",
+	    "Polargeist",
+	    "level_3",
+	    "Step" 
+    ],
+    [
+        "dry_out",
+	    "Dry Out",
+	    "level_4",
+	    "DJVI" 
+    ],
+    [
+        "base_after_base",
+	    "Base After Base",
+	    "level_5",
+	    "DJVI" 
+    ],
+    [
+        "cant_let_go",
+	    "Can't Let Go",
+	    "level_6",
+	    "DJVI" 
+    ],
+    [
+        "jumper",
+	    "Jumper",
+	    "level_7",
+	    "Waterflame" 
+    ]
+]
---
+++ assets/scripts/game/allLevels.js
@@ -0,0 +1,44 @@
+window.allLevels = [
+    [
+        "stereo_madness", // internal level name
+	    "Stereo Madness", // proper level name
+	    "level_1",        // level id in assets/levels
+	    ["RobTop", "Forever Bound"]   // person who made the level, person who made the song
+    ],
+    [
+        "back_on_track",
+	    "Back On Track",
+	    "level_2",
+	    ["RobTop", "DJVI" ]
+    ],
+    [
+        "polargeist",
+	    "Polargeist",
+	    "level_3",
+	    ["RobTop", "Step"]
+    ],
+    [
+        "dry_out",
+	    "Dry Out",
+	    "level_4",
+	    ["RobTop", "DJVI" ]
+    ],
+    [
+        "base_after_base",
+	    "Base After Base",
+	    "level_5",
+	    ["RobTop", "DJVI"]
+    ],
+    [
+        "cant_let_go",
+	    "Cant Let Go",
+	    "level_6",
+	    ["RobTop", "DJVI"] 
+    ],
+    [
+        "jumper",
+	    "Jumper",
+	    "level_7",
+	    ["RobTop", "Waterflame"]
+    ]
+]
